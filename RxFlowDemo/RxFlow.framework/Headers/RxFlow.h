240ca6011dbb5e07c9eb462f00973de882dd79e5
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2018-03-25 17:41:55 -0400

    fix: prevent a 'unowned reference' reading error

---
+++ RxFlowDemo/RxFlow.framework/Headers/RxFlow.h
@@ -0,0 +1,19 @@
+//
+//  RxFlow.h
+//  RxFlow
+//
+//  Created by Thibault Wittemberg on 17-12-30.
+//  Copyright (c) RxSwiftCommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxFlow.
+FOUNDATION_EXPORT double RxFlowVersionNumber;
+
+//! Project version string for RxFlow.
+FOUNDATION_EXPORT const unsigned char RxFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFlow/PublicHeader.h>
+
+
