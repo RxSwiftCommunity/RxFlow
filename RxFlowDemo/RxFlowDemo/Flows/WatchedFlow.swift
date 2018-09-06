14c76f20efc4dce75b964918c9675e3229cf5d94
Alex Moisei <alexandru.moisei@icloud.com>
2018-09-07 01:47:56 +0200

    UIViewController dismissed ControlEvent fix (#75)

--- RxFlowDemo/RxFlowDemo/Flows/WatchedFlow.swift
+++ RxFlowDemo/RxFlowDemo/Flows/WatchedFlow.swift
@@ -21,6 +21,10 @@ class WatchedFlow: Flow {
         self.services = services
     }
 
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+
     func navigate(to step: Step) -> NextFlowItems {
 
         guard let step = step as? DemoStep else { return NextFlowItems.none }
