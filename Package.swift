188833fae1cc3b5555f0ed07efc148964e16556d
antonnazarov <nazarof.anton@gmail.com>
2019-09-05 21:39:50 -0700

    Remove tvOS and macOS

--- Package.swift
+++ Package.swift
@@ -4,9 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "RxFlow",
-    platforms: [
-        .iOS(.v9), .tvOS(.v9), .macOS(.v10_11)
-    ],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "RxFlow", targets: ["RxFlow"]),
     ],
