7d0c449957b030228c28c3e578968a65fc236a5f
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2021-08-16 11:49:06 +0200

    project: bump RxSwift 6.2.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "RxFlow", targets: ["RxFlow"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.2.0")),
     ],
     targets: [
         .target(
