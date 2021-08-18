1dcdefed3d24acf9473836f803b2911709887a5a
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2021-08-18 17:43:48 +0200

    fix: reentrancy issue with forwardToCurrentFlow

--- RxFlow/FlowCoordinator.swift
+++ RxFlow/FlowCoordinator.swift
@@ -128,7 +128,9 @@ public final class FlowCoordinator: NSObject {
     private func performSideEffects(with flowContributor: FlowContributor) {
         switch flowContributor {
         case let .forwardToCurrentFlow(step):
-            self.stepsRelay.accept(step)
+            DispatchQueue.main.async { [weak self] in
+                self?.stepsRelay.accept(step)
+            }
         case let .forwardToParentFlow(step):
             parentFlowCoordinator?.stepsRelay.accept(step)
         case .contribute:
